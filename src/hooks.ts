b91103e11a92818bc8d2395477ad70de20ab8bee
Andrej Bartko <andrej.bartko@gmail.com>
2021-04-19 15:54:29 +0100

    doc update

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,35 +1,5 @@
 /*
- * @desc
- * simple side effects for redux
- * this effect pass all the actions further in the pipe
- * it also triggers the hook when the action type matches
- * the one registered
- * in the side effect the callback has access to dispatch and getState
- * the middleware is synchrounous only
- * as first think it calls next(action) which synchrounously run the rest of the middleware
- * and will update the store.
- * so getState function will return only the next state not previous.
- *
- * when dispatching an action from this middleware it will be dispatched
- * only after the action to which the effected is hooked is finalized
- *
- * usage:
- * if using redux-thunk
- * this need to go after thunk in the pipeline
- * if you use middleware to manage async action this need to go after
- * in the pipeline
- *
- * ideally hooks will sit in separate file
- * hooks.ts (hooks.js)
- *
- * and will be called on the beginning of dedicated reducer
- * in reducer:
- * import './hooks'
- * which will execute the hooks once.
- *
- * ofType(ActionTypes.PDF_DOCUMENT_FOLDER_SELECTED, (action: AnyAction) => {
- *   dispatch(newAction(action.payload.pick));
- * })
+ * 
 */
 
 import {
