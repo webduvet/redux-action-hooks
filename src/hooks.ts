f0c4692688783be77eace9de9d83e843734c929b
Andrej Bartko <andrej.bartko@gmail.com>
2021-10-17 20:39:52 +0100

    work in progress

--- src/hooks.ts
+++ src/hooks.ts
@@ -27,18 +27,23 @@ let counter = 0;
  *
  * @return void
  */
-export default (store: Store) => {
-  // reset the container whenever we applyMiddleware
-  // NOTE: this supports only single store app
-  _container.hooks = {};
-  return (next: Function) => (action: AnyAction) => {
-    // NOTE
-    // by calling the action right here we execute the synchrounous flow BEFORE our hook
-    // effectively triggering our hooks as last in the remaining queue
-    next(action);
-    (_container.hooks[action.type] || [])
-    .forEach((hook: ActionHook) => {
-      hook({ action, getState: store.getState, dispatch: store.dispatch });
-    });
-  };
+export function effectFactory() {
+  const container: Hooks = {}
+  return (store: Store) => {
+    // reset the container whenever we applyMiddleware
+    // NOTE: this supports only single store app
+    return (next: Function) => (action: AnyAction) => {
+      // NOTE
+      // by calling the action right here we execute the synchrounous flow BEFORE our hook
+      // effectively triggering our hooks as last in the remaining queue
+      next(action);
+      (container[action.type] || [])
+      .forEach((hook: ActionHook) => {
+        hook({ action, getState: store.getState, dispatch: store.dispatch });
+      });
+    };
+  }
+
 }
+
+export default effectFactory;
